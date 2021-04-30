c464ec3db8c4d54bfa3e4423475210de35701628
Reeshita Paul <77070824+reeshi-18@users.noreply.github.com>
2021-05-01 01:51:33 +0530

    Complexity analysis + Basics Warmup

---
+++ TEST.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n=0;
+	do
+	{
+	    cin>>n;
+	    if(n==42)
+	    {
+	        break;
+	    }
+	    cout<<n<<endl;
+	    
+	}while(1);
+	return 0;
+}
