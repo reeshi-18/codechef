f6b818ed88648680304bc99a4863debc691ba784
Reeshita Paul <77070824+reeshi-18@users.noreply.github.com>
2021-05-01 14:29:32 +0530

    Mathematics in Competitive Programming

---
+++ CHEFSQR.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main () {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        ll a, b;
+        int c=0;
+        ll ans = -1;
+        for (int i = sqrt(n); i>=1; i--) 
+		{
+            if (n % i == 0 && i!=(n/i))
+			{
+                a = (n/i + i)/2;
+                b = (n/i - i)/2;
+                if (b*b + n == a*a) 
+				{
+                    c=1;
+                    ans=b;
+                    break;
+                }
+            }
+        }
+        if(c) 
+		{
+            ans = ans * ans;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
